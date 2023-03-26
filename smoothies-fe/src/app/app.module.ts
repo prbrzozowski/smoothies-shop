6a62f4e98f6263bc0bbd0fabd4724098279c81b0
Przemysław Brzozowski <pbrzozowski@gmail.com>
2023-03-26 18:05:40 +0200

    Init

--- smoothies-fe/src/app/app.module.ts
+++ smoothies-fe/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import {BusinessOwnerModule} from './modules/businessowner/businessowner.module'
 
 @NgModule({
   declarations: [
@@ -10,7 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserModule,
+    ButtonModule,
+    TableModule,
+    BusinessOwnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ smoothies-fe/src/app/modules/businessowner/businessowner.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from "@angular/common";
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [
+
+    ],
+    imports: [
+        CommonModule,
+        ButtonModule,
+        TableModule
+    ],
+    providers: [],
+
+  })
+  export class BusinessOwnerModule { }
